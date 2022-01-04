7c5bef583a4811dc7e90dbac31aeb20e1b18e29d
Morisaki Mikiya <git@morisakimikiya.com>
2022-01-04 12:35:39 +0000

    chore: prettier error test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
