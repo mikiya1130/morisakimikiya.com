be0ddb0e95860987098ad8a362fd108b564cdd17
Morisaki Mikiya <git@morisakimikiya.com>
2022-01-06 00:46:17 +0000

    feat: BoxProps & CustomProps

--- src/components/ContentBox.tsx
+++ src/components/ContentBox.tsx
@@ -1,16 +1,18 @@
-import { Box, Center, Heading, Icon } from '@chakra-ui/react';
+import { Box, BoxProps, Center, Heading, Icon } from '@chakra-ui/react';
 import { ReactNode } from 'react';
 import { IconType } from 'react-icons';
 
-type Props = {
+type CustomProps = {
   icon?: IconType;
   title?: string;
   children: ReactNode;
 };
 
-const ContentBox = ({ icon, title, children }: Props) => {
+type Props = BoxProps & CustomProps;
+
+const ContentBox = ({ icon, title, children, ...boxProps }: Props) => {
   return (
-    <Box w='full' p={4} borderRadius={20} bg='white' color='black'>
+    <Box w='full' p={4} borderRadius={20} bg='white' color='black' {...boxProps}>
       {title != undefined && (
         <Heading mb={4}>
           <Center>
