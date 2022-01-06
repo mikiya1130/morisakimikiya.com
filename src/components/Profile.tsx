780f3043efa639f9851855e27a0e5d160de8fd05
Morisaki Mikiya <git@morisakimikiya.com>
2022-01-06 10:46:33 +0000

    feat: RedRoundButton

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,11 +1,13 @@
 import { Image } from '@chakra-ui/react';
 import { MdPerson } from 'react-icons/md';
 import ContentBox from '@/components/ContentBox';
+import RedRoundButton from '@/components/RedRoundButton';
 
 const Profile = () => {
   return (
     <ContentBox icon={MdPerson} title='PROFILE'>
       <Image boxSize='200px' borderRadius='20px' src='images/profile.jpg' alt='profile' />
+      <RedRoundButton>more</RedRoundButton>
       <Image w='200px' h='300px' src='images/instruments.png' alt='profile' />
     </ContentBox>
   );
