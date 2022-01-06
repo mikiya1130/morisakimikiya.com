742f6313e03f182efdd6ba16b1f19ee4ead3604a
Morisaki Mikiya <git@morisakimikiya.com>
2022-01-06 10:46:02 +0000

    feat: profile & instruments image

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,11 +1,12 @@
-import { Text } from '@chakra-ui/react';
+import { Image } from '@chakra-ui/react';
 import { MdPerson } from 'react-icons/md';
 import ContentBox from '@/components/ContentBox';
 
 const Profile = () => {
   return (
     <ContentBox icon={MdPerson} title='PROFILE'>
-      <Text>profile</Text>
+      <Image boxSize='200px' borderRadius='20px' src='images/profile.jpg' alt='profile' />
+      <Image w='200px' h='300px' src='images/instruments.png' alt='profile' />
     </ContentBox>
   );
 };
