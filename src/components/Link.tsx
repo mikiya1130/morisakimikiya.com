6e1aefd379c1390c7c89af3896f999131a711aaf
Morisaki Mikiya <git@morisakimikiya.com>
2022-01-08 06:59:46 +0000

    feat: Link

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,11 +1,30 @@
-import { Text } from '@chakra-ui/react';
+import { Box, Heading, SimpleGrid, Link as ChakraLink } from '@chakra-ui/react';
 import { MdLink } from 'react-icons/md';
 import ContentBox from '@/components/ContentBox';
+import { linkList, LinkType } from '@/data/link';
 
 const Link = () => {
   return (
     <ContentBox icon={MdLink} title='LINK'>
-      <Text>link</Text>
+      <SimpleGrid minChildWidth='250px' spacing={4}>
+        {linkList.map((link: LinkType, index: number) => (
+          <Box
+            key={index}
+            p={2}
+            border='1px solid'
+            borderColor='black'
+            borderRadius='20px'
+            boxShadow='0px 1px 1px 0px'
+          >
+            <Heading as='h4' size='md' textAlign='center'>
+              <ChakraLink href={link.link} isExternal color='red'>
+                {link.title}
+              </ChakraLink>
+            </Heading>
+            {link.content}
+          </Box>
+        ))}
+      </SimpleGrid>
     </ContentBox>
   );
 };
