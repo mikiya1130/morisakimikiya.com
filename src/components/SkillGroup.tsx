246bb28a433f52e4b5895618f332987fb56ee25c
Mikiya Morisaki <git@morisakimikiya.com>
2025-12-14 23:35:51 +0900

    update: profile & skill

--- src/components/SkillGroup.tsx
+++ src/components/SkillGroup.tsx
@@ -5,15 +5,16 @@ import { SkillType } from '@/data/skill';
 type Props = {
   name: string;
   skillList: SkillType[];
+  cols: number;
 };
 
-const SkillGroup = ({ name, skillList }: Props) => {
+const SkillGroup = ({ name, skillList, cols }: Props) => {
   return (
     <>
       <Heading as='h3' my={4} size='lg'>
         {name}
       </Heading>
-      <SimpleGrid minChildWidth='250px' spacing={4}>
+      <SimpleGrid minChildWidth={`${750 / cols}px`} spacing={4}>
         {skillList.map((skill: SkillType, index: number) => (
           <SkillGrid key={index} title={skill.title} logo={skill.logo} star={skill.star}>
             {skill.content}
